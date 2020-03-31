5dd371e89ea2985b95e77e7b0575f659656618cf
Josue Diaz <jdiaz77711@gmail.com>
2020-03-31 16:58:17 +0100

    remove company logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,6 @@
             role="navigation"
             aria-label="main navigation"
         >
-
-            <nuxt-link
-                to="/"
-                class="navbar-brand mr-8 py-2"
-            >
-                <img
-                    class="default__juicee-logo"
-                    src="~/assets/images/default/juicee-logo.svg"
-                    alt="juicee logo"
-                >
-            </nuxt-link>
             <nuxt-link
                 class="navbar-item capitalize p-2 hover:bg-gray-200 transition-all duration-150 ease"
                 to="/single-price-grid"
